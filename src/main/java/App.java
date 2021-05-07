b8986f600c0a4dfa99344f69fafb183c0ee96361
Li Maxim <FrenkLee365@gmail.com>
2021-05-07 16:09:48 +0900

    commit 1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,17 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        Cat bean3 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean3.getMessage());
+        Cat bean4 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean3.getMessage());
+        boolean comp1 = bean == bean2;
+        boolean comp2 = bean3 == bean4;
+        System.out.println(comp1 + " " + comp2);
+
     }
 }
